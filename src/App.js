3a4078a50a414a091aedd0043e624e649d6ff3d2
gawhar <gawharsalman@outlook.com>
2024-11-29 00:20:07 +0530

    deleted:    public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	deleted:    src/App.css 	modified:   src/App.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [grocery, setGrocery] = useState("");
+
+  // check local storage.
+  const getSavedGroceryList = () => {
+    const groceryList = JSON.parse(localStorage.getItem("savedGroceryList"));
+    if (groceryList) {
+      return groceryList;
+    } else {
+      return [];
+    }
+  };
+
+  const [groceryList, setGroceryList] = useState(getSavedGroceryList());
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const id = new Date().getTime().toString();
+    const groceryDict = { id, grocery };
+    setGroceryList([...groceryList, groceryDict]);
+    localStorage.setItem("savedGroceryList", JSON.stringify(groceryList));
+    setGrocery("");
+  };
+
+  const deleteItemHandler = (id) => {
+    const updatedGroceryList = groceryList.filter((item) => id !== item.id);
+    setGroceryList(updatedGroceryList);
+    localStorage.setItem(
+      "savedGroceryList",
+      JSON.stringify(updatedGroceryList)
+    );
+  };
+
+  // Update local storage every time when user submit data.
+  useEffect(() => {
+    localStorage.setItem("savedGroceryList", JSON.stringify(groceryList));
+  }, [groceryList]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="groceryName"></label>
+        <input
+          type="text"
+          id="groceryName"
+          value={grocery}
+          onChange={(event) => setGrocery(event.target.value)}
+        ></input>
+        <button type="submit">Save</button>
+      </form>
+      <ul>
+        {groceryList.map((item) => {
+          return (
+            <li key={item.id}>
+              <div>
+                <Checkbox></Checkbox>
+                <p>{item.grocery}</p>
+              </div>
+              <button onClick={() => deleteItemHandler(item.id)}>
+                <span className="material-symbols-outlined">remove</span>
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
   );
-}
+};
+
+const Checkbox = () => {
+  const [isChecked, setIsChecked] = useState(false);
+  return (
+    <input
+      className="checkbox"
+      type="checkbox"
+      checked={isChecked}
+      onChange={() => setIsChecked(!isChecked)}
+    ></input>
+  );
+};
 
 export default App;
